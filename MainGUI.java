7bb63256e4f5c590685ad49af51a3a7ba39d636f
Dylan Chen <hjchen2005@gmail.com>
2015-11-18 02:03:27 -0500

    Update MainGUI.java
    add window closing events; add picture in the middle

--- MainGUI.java
+++ MainGUI.java
@@ -9,24 +9,53 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
 import javafx.scene.image.ImageView;
 import javafx.scene.image.Image;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.KeyCode;
+import javafx.scene.layout.BorderPane;
+import javax.swing.JOptionPane;
 
 public class MainGUI extends Application {
     private static Log log=LogFactory.getLog(MainGUI.class);
+    Stage window;
     @Override
     public void start(Stage primaryStage) {
-       try{
-            Image image = new Image("Image/questionmark.jpg");
+        window=primaryStage;
+        BorderPane borderPane = new BorderPane();
+        StackPane root = new StackPane();
+        Image image = new Image("file:Image/questionmark.jpg");
+        //try{
+            
             ImageView imageView=new ImageView();
             imageView.setImage(image);
-            imageView.setFitWidth(75);
+            //imageView.setFitWidth(75);
             imageView.setPreserveRatio(true);
-        } catch(Exception ex){
-            log.error("oops!", ex.getCause()) ;
-        }
+            imageView.fitWidthProperty().bind(primaryStage.widthProperty()); 
+            //root.getChildren().add(imageView);
+       // }catch(Exception ex){
+         //   log.error("oops!", ex.getCause()) ;
+        //}
+       Label label1 = new Label("Name:");
+        TextField textField = new TextField ();
+        textField.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent keyEvent) {
+                if (keyEvent.getCode() == KeyCode.ENTER)  {
+                    String name = textField.getText(); //send this name to GuessGameGUI
+                // clear text
+                textField.setText("");
+            }
+           }
+        });
+        HBox hb = new HBox();
+        hb.getChildren().addAll(label1, textField);
+        hb.setSpacing(10);
         Button btn = new Button();
         btn.setText("Enter game");
         btn.setOnAction(new EventHandler<ActionEvent>() {
@@ -34,7 +63,7 @@ public class MainGUI extends Application {
             @Override
             public void handle(ActionEvent event) {
                 try{
-                    Configuration myConfig = new Configuration(16, 4);
+                    Configuration myConfig = new Configuration(16, 8);
 
                 // Resolve and inject dependencies.
                     IChooser myChooser = new RandomChooser(myConfig);
@@ -49,15 +78,22 @@ public class MainGUI extends Application {
               }
             });
         
-        StackPane root = new StackPane();
+        
         //root.getChildren().add(imageView);
-        root.getChildren().add(btn);
+      
+        //root.getChildren().addAll(imageView,hb,btn);
+        borderPane.setCenter(imageView);
+        borderPane.setTop(hb);
+        borderPane.setBottom(btn);
+        Scene scene = new Scene(borderPane, 400, 600);
         
-        Scene scene = new Scene(root, 400, 250);
-        
-        primaryStage.setTitle("Guessing Game");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        window.setTitle("Guessing Game");
+        window.setScene(scene);
+        window.setOnCloseRequest(e->{
+            e.consume();
+            closeProgram();
+        });
+        window.show();
         
     }
     /**
@@ -67,4 +103,12 @@ public class MainGUI extends Application {
         launch(args);
         
     }    
+
+    private void closeProgram() {
+        int dialogButton = JOptionPane.YES_NO_OPTION;
+        JOptionPane.showConfirmDialog (null,"Do you want to close");
+        if(dialogButton == JOptionPane.YES_OPTION)
+            window.close();
+    }
 }
+
